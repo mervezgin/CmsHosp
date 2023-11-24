316746db29242ce8772e7045df3fa2739bc1d062
Merve <mervezgin@gmail.com>
2023-11-24 18:20:10 +0300

    Added Login and Verify Method

--- src/CmsHosp/CmsHosp.Api/Controllers/AccountController.cs
+++ src/CmsHosp/CmsHosp.Api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using CmsHosp.Data;
 using CmsHosp.Data.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 
 namespace CmsHosp.Api.Controllers
@@ -17,6 +18,7 @@ namespace CmsHosp.Api.Controllers
         {
             _appDbContext = appDbContext;
         }
+
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegisterRequest request)
         {
@@ -41,12 +43,59 @@ namespace CmsHosp.Api.Controllers
             await _appDbContext.SaveChangesAsync();
 
             return Ok("Kullanıcı başarılı bir şekilde kayıt oldu.");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+            if (user == null)
+            {
+                return BadRequest("Kullanıcı bulunamadı.");
+            }
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Şifre Yanlış.");
+            }
+
+            if (user.VerifiedAt == null)
+            {
+                return BadRequest("Kullanıcı onaylanmadı.");
+            }
+
+            return Ok($"Tekrar Hoş geldiniz, {user.Name}");
+            
+        }
+
+        [HttpPost("verify")]
+        public async Task<IActionResult> Verify(string token)
+        {
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
+            if (user == null)
+            {
+                return BadRequest("Geçersiz token.");
+            }
+
+            user.VerifiedAt = DateTime.Now;
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok("Kullanıcı doğrulandı.");
 
         }
 
         private string CreateRandomToken()
         {
             return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
